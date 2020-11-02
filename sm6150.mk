7062f9ea49ff5979fb1694eab52228b7670f87bb
Arian <arian.kulmer@web.de>
2020-11-02 23:37:22 +0100

    sm6150-common: Use QTI common wfd component
    Change-Id: I311318565e0780ae2b509e761021ce8a2737c5ee

--- sm6150.mk
+++ sm6150.mk
@@ -259,7 +259,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     av \
     bt \
     perf \
-    overlay
+    overlay \
+    wfd
 
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 
@@ -378,13 +379,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libavservices_minijail_32 \
-    libdisplayconfig.qti \
-    libnl \
-    libqdMetaData
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
